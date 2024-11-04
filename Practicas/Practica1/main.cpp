bc64b021f7cc0778d255ecea2ce756a7843d559f
acostacion <carmengb27@ucm.es>
2024-11-04 15:58:31 +0300

    I told you we needed more glitter

--- Practicas/Practica1/main.cpp
+++ Practicas/Practica1/main.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	try {
+	/*try {
 		Game game;
 		game.run();
 	}
 	catch (const std::string& msg) {
 		cerr << "Error: " << msg << endl;
-	}
+	}*/
 
 	return 0;
 }
