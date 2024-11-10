061c1ef1c85adedabef36ade3638bf6edc5db8bb
MaEduardaB <mbeckers@ucm.es>
2024-11-10 18:05:47 +0100

    colli

--- Practicas/Practica1/Collision.h
+++ Practicas/Practica1/Collision.h
@@ -2,8 +2,12 @@
 class Collision {
 public:
 	struct collision {
+
 		bool collides = false; // con tilemap
 		bool damages = false; // player con enemigo collides
+		bool fromEnemy = false;
+		bool fromMushroom = false;
+
 		SDL_Rect intersectRect;
 
 		operator bool() const { return collides; }
