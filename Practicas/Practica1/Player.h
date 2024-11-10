3eef01975d30fec71b69fcbd2d94f48a9d61cca0
MaEduardaB <mbeckers@ucm.es>
2024-11-10 18:16:15 +0100

    player h

--- Practicas/Practica1/Player.h
+++ Practicas/Practica1/Player.h
@@ -9,29 +9,63 @@ class Game;
 
 class Player{
 private:
-	
+#pragma region References
 	Game* game;
-	SDL_Renderer* renderer;
 	Texture* texturaMario;
 	Texture* texturaSMario;
-	
+#pragma endregion
+
+#pragma region Movement
+
 	Point2D<float> dir;
 	Point2D<float> pos;
 	Point2D<float> posInicio;
-	
-	SDL_Rect collider;
-	Collision::collision collisionRes;
-
-	int anim, invulnerableTimer; // indice del frame de la animacion
-	int life;
-	int margenColi = 1;
 	float verticalVelocity;
-	bool superMario = false, invulnerable;
 	bool isGrounded; // si esta en el suelo
 	bool movingDer; // si se esta moviendo a la derecha, se usa en el game para saber si mueve el mapoffset y que no se mueva infinitamente si esta en ese borde
 	bool moving; // si se mueve para la animacion
-	
+
+#pragma endregion
+
+#pragma region Collision
+
+	SDL_Rect collider;
+	Collision::collision collisionRes;
+
+#pragma endregion
+
+#pragma region Animation
+
+	int anim; // indice del frame de la animacion
 	SDL_RendererFlip flip; // para que se gire horizontalmente la animacion
+
+#pragma endregion
+
+#pragma region Atributes
+
+	int invulnerableTimer;
+	int life;
+	int margenColi = 1;
+	bool superMario = false;
+	bool invulnerable;
+
+#pragma endregion
+
+	
+#pragma region Methods
+
+	SDL_Rect createRect(float, float, float, float);
+	void resetPos();
+	SDL_Rect getCollider(bool forRender) const;
+	SDL_Rect getNextCollider(int x, int y) const;
+	void animation();
+	void change();
+	void jump();
+	void decreaseLife();
+	
+#pragma endregion
+
+#pragma region Constants
 
 	//CONSTANTES.
 	// En sdl ir para arriba es negativo e ir para abajo es positivo.
@@ -42,14 +76,14 @@ private:
 	const float COLLISION_OFFSET = 0.3f; // velocidad de movimiento.
 	const float GROUND_LEVEL = 12.9f;
 	const float GRAVITY = 1.0f; // gravedad.
-	
-	// Submétodos.
-	SDL_Rect createRect(float, float, float, float);
-	void resetPos();
+
+#pragma endregion
+
+
 public:
 	Player(Game*, std::istream&);
 
-	void render();
+	void render(SDL_Renderer* renderer);
 	void update();
 	void handleEvent(SDL_Event);
 	Collision::collision hit(const SDL_Rect otherRect);
@@ -59,6 +93,39 @@ public:
 	Point2D<float> getPos() const { return this->pos; }
 	bool getMovingDer()const { return movingDer; }
 	Point2D<float> getDir()const { return dir; }
-	bool isSuperMario()const { return this->superMario; } // obtener si es superMario o no.
+	bool isSuperMario()const; // obtener si es superMario o no.
+
+	//metodos que devuelven la posicion y direccion para detectar colisiones
+	Point2D<float> getPlayerPos();
+	Point2D<float> getPlayerDir();
+
+	//metodo auxiliare que devuelve si es un supermario
+	bool esSuperMario() const;
+
+	int getPlayerLife() const;
+
+	void resetPlayerPos();
+
 };
+
+
+inline bool
+Player::isSuperMario() const {
+	return superMario;
+}
+
+inline Point2D<float>
+Player::getPlayerPos() {
+	return pos;
+}
+
+inline Point2D<float>
+Player::getPlayerDir() {
+	return dir;
+}
+inline int
+Player::getPlayerLife() const {
+	return life;
+}
+
 
